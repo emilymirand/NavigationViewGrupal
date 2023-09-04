14ed3f37d447db4428a37dcb483422dd616aae13
emilymirand <125322859+emilymirand@users.noreply.github.com>
2023-09-03 21:52:06 -0500

    complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# NavigationView
+![image](https://user-images.githubusercontent.com/107895582/216540062-b447dede-1bf1-4b30-bf7c-957c2d1326e3.png)
+![image](https://user-images.githubusercontent.com/107895582/216540079-32831d9b-8716-4309-8e47-681798dcac00.png)
---
+++ app/src/main/java/com/example/navigationview/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.navigationview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarNavView);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.iconhome);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navView = (NavigationView)findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        boolean fragmentTransaction = false;
+
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.menu_seccion_1:
+                fragment = new FragmentCliente();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_2:
+                fragment = new BlankFragmentFactura();
+                fragmentTransaction = true;
+                break;
+
+
+        }
+
+        if(fragmentTransaction) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            menuItem.setChecked(true);
+            getSupportActionBar().setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawers();
+        return false;
+    }
+    public void onClick(View view) {
+        Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+}
